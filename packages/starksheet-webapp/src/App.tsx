59725bee22aca1f7f37deea5e8a863277e2e0efc
Georges Biaux <georges.biaux@gadz.org>
2022-07-02 18:15:31 +0200

    Style scrollbar

--- packages/starksheet-webapp/src/App.tsx
+++ packages/starksheet-webapp/src/App.tsx
@@ -3,7 +3,7 @@ import Header from "./components/Header/Header";
 import { Box } from "@mui/material";
 import { StarknetContextProvider } from "./contexts/StarknetContext";
 import SheetTable from "./components/SheetTable/SheetTable";
-import { CELL_BORDER_WIDTH } from "./config";
+import { CELL_BORDER_WIDTH, CELL_HEIGHT } from "./config";
 import Footer from "./components/Footer/Footer";
 
 function App() {
@@ -17,8 +17,27 @@ function App() {
         <SheetTable
           sx={{
             marginTop: `-${CELL_BORDER_WIDTH}px`,
+            marginBottom: `-${CELL_BORDER_WIDTH}px`,
             overflow: "auto",
             flex: 1,
+            "&::-webkit-scrollbar": {
+              width: `${CELL_HEIGHT}px`,
+              height: `${CELL_HEIGHT}px`,
+              backgroundColor: "#C6D2E4",
+              border: `${CELL_BORDER_WIDTH}px solid black`,
+              boxShadow: `inset -5px -5px 3px #DCE3ED, inset 5px 5px 3px #949EAC`,
+            },
+            "&::-webkit-scrollbar-thumb": {
+              backgroundColor: "#C6D2E4",
+              border: `${CELL_BORDER_WIDTH}px solid black`,
+              boxShadow: `inset 5px 5px 3px #DCE3ED, inset -5px -5px 3px #949EAC`,
+              cursor: "pointer",
+            },
+            "&::-webkit-scrollbar-corner": {
+              backgroundColor: "#C6D2E4",
+              border: `${CELL_BORDER_WIDTH}px solid black`,
+              boxShadow: `inset 5px 5px 3px #DCE3ED, inset -5px -5px 3px #949EAC`,
+            },
           }}
         />
         <Footer />

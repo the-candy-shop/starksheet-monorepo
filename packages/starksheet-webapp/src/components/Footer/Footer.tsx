72678bd93c28f14c75cbdeb474061aa7437670c7
Georges Biaux <georges.biaux@gadz.org>
2022-07-06 15:51:07 +0200

    Style footer

--- packages/starksheet-webapp/src/components/Footer/Footer.tsx
+++ packages/starksheet-webapp/src/components/Footer/Footer.tsx
@@ -10,9 +10,26 @@ export type FooterProps = {
 function Footer({ sx }: FooterProps) {
   return (
     <Box sx={{ display: "flex", ...sx }}>
-      <GreyCell>Sheet 1</GreyCell>
-      <GreyCell sx={{ marginLeft: `-${CELL_BORDER_WIDTH}px` }}>
-        Sheet 2 (Coming Soon)
+      <GreyCell
+        sx={{ width: "146px", "& .content": { justifyContent: "center" } }}
+      >
+        Sheet 1
+      </GreyCell>
+      <GreyCell
+        sx={{
+          marginLeft: `-${CELL_BORDER_WIDTH}px`,
+          width: "345px",
+          "& .content": {
+            justifyContent: "center",
+            color: "rgba(0,0,0,0.5)",
+          },
+        }}
+      >
+        Sheet 2 (
+        <a href="#" target="_blank">
+          Coming Soon
+        </a>
+        )
       </GreyCell>
       <GreyCell sx={{ flex: 1, marginLeft: `-${CELL_BORDER_WIDTH}px` }} />
       <GreyCell sx={{ marginLeft: `-${CELL_BORDER_WIDTH}px` }}>i</GreyCell>

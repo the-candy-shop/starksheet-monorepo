001ef20b6957b85986bd787f659b5e831364693f
Clément Walter <clement0walter@gmail.com>
2022-08-05 19:41:29 +0200

    Draw 20 winners amongst all followers

--- packages/starksheet-cairo/notebooks/1k_followers_draw.py
+++ packages/starksheet-cairo/notebooks/1k_followers_draw.py
@@ -10,6 +10,11 @@ client = tweepy.Client(os.environ["TWITTER_TOKEN"])
 
 starksheet_account = client.get_user(username="Starksheet")
 response = client.get_users_followers(starksheet_account[0].id, max_results=1000)
-pd.DataFrame(response[0]).sample(n=20, replace=False).assign(
+last = client.get_users_followers(
+    starksheet_account[0].id,
+    max_results=1000,
+    pagination_token=response[3]["next_token"],
+)
+pd.DataFrame(response[0] + last[0]).sample(n=20, replace=False).assign(
     username=lambda df: "@" + df.username
 )["username"].to_csv("followers_drawn.csv", index=False)

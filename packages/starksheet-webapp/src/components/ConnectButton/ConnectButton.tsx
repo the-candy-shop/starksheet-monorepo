b4d84344d14e71c8a9ea9d31c5f2dbff035be1e9
Clément Walter <clement0walter@gmail.com>
2022-10-06 10:05:17 +0200

    Show snackbar warning when wrong network detected

--- packages/starksheet-webapp/src/components/ConnectButton/ConnectButton.tsx
+++ packages/starksheet-webapp/src/components/ConnectButton/ConnectButton.tsx
@@ -1,7 +1,9 @@
 import { BoxProps } from "@mui/material";
 import { disconnect, getStarknet } from "get-starknet";
+import { useSnackbar } from "notistack";
 import { useContext } from "react";
 import { AccountContext } from "../../contexts/AccountContext";
+import { hex2str } from "../../utils/hexUtils";
 import Button from "../Button/Button";
 
 export type ConnectButtonProps = {
@@ -10,12 +12,26 @@ export type ConnectButtonProps = {
 
 function ConnectButton({ sx }: ConnectButtonProps) {
   const { accountAddress, setAccountAddress } = useContext(AccountContext);
+  const { enqueueSnackbar } = useSnackbar();
 
   const onClick = async () => {
     if (getStarknet().isConnected) {
       disconnect();
     }
     const selected = await getStarknet().enable({ showModal: true });
+    const chainId = hex2str(getStarknet().account.chainId);
+    if (
+      !process.env.REACT_APP_NETWORK?.includes(chainId.slice(3).toLowerCase())
+    ) {
+      enqueueSnackbar(
+        `Wrong network detected: "${chainId
+          .slice(3)
+          .toLowerCase()}" instead of "${
+          process.env.REACT_APP_NETWORK?.split("-")[1]
+        }"`,
+        { variant: "warning" }
+      );
+    }
     setAccountAddress(selected[0]);
   };
 

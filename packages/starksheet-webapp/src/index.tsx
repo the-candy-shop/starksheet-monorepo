9f0c6bb01685a3cd56b146d9b8274b433f284416
Robin Straub <robin@brumaire.io>
2023-06-05 19:16:23 +0200

    feat: disable strict mode

--- packages/starksheet-webapp/src/index.tsx
+++ packages/starksheet-webapp/src/index.tsx
@@ -38,9 +38,7 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+  <RouterProvider router={router} />
 );
 
 function Root() {
